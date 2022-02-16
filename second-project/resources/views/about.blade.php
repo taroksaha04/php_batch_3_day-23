6ad217a46db0b472464894138f68c12c7e979c57
taroksaha04 <taroksaha04@gmail.com>
2022-02-16 13:08:23 +0600

    day23finalUpdated

---
+++ second-project/resources/views/about.blade.php
@@ -0,0 +1,19 @@
+{{--<h1>This is about page</h1>--}}
+{{--<a href="{{ url('/') }}">Home</a>--}}
+{{--<a href="{{ url('/bitm')}}">BITM</a>--}}
+{{--<a href="{{ url('/about') }}">About</a>--}}
+{{--<a href="{{ url('/contact') }}">Contact</a>--}}
+
+
+{{--mastering --}}
+
+@extends('master')
+
+@section('title')
+    About Page
+@endsection
+
+@section('body')
+    <h1>This is About Page</h1>
+@endsection
+
---
+++ second-project/resources/views/demo.blade.php
@@ -0,0 +1,15 @@
+{{--<h1>This is Bitm</h1>--}}
+{{--<a href="{{ url('/') }}">Home</a>--}}
+{{--<a href="{{ url('/bitm')}}">BITM</a>--}}
+{{--<a href="{{ url('/about') }}">About</a>--}}
+{{--<a href="{{ url('/contact') }}">Contact</a>--}}
+
+@extends('master')
+
+@section('title')
+    BITM Page
+@endsection
+
+@section('body')
+    <h1>This is Bitm Page</h1>
+@endsection
